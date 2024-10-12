5f39b6394e852b1def4b81649cc998688252b5db
comma <comma0224@gmail.com>
2024-10-12 20:18:24 +0900

    start

---
+++ src/test/java/com/comma/CommaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
