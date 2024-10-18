ffc2be8a0d6e916e135509057fa4da2739a0b804
comma <comma0224@gmail.com>
2024-10-18 22:31:11 +0900

    현재

---
+++ src/main/java/com/comma/common/controller/InfoController.java
@@ -0,0 +1,35 @@
+package com.comma.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InfoController {
+
+
+    @GetMapping("/introduce")
+    public String introduce() {
+        return "info/introduce";
+    }
+
+    @GetMapping("/ad-guide")
+    public String adGuide() {
+        return "info/ad-guide";
+    }
+
+    @GetMapping("/terms")
+    public String terms() {
+        return "info/terms";
+    }
+
+    @GetMapping("/privacy")
+    public String privacy() {
+        return "info/privacy";
+    }
+
+    @GetMapping("/youth")
+    public String youth() {
+        return "info/youth";
+    }
+
+}
